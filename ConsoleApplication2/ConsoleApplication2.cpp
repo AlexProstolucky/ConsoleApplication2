0f74df8cdb1514a16ef2d7f3c5e357ac6962d220
admin1 <admin1@ADMIN1-PC36>
2022-07-19 19:48:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+
+int fibonacci(int number)
+{
+	if (number == 0)
+		return 0; 
+	if (number == 1)
+		return 1; 
+	return fibonacci(number - 1) + fibonacci(number - 2);
+}
+
+
+int main()
+{
+	for (int count = 0; count < 13; ++count)
+		cout << fibonacci(count) << " ";
+
+	return 0;
+}
